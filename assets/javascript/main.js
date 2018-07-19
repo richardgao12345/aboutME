fb9209beb37121b322a4f65de5711ffc3d41590d
richardgao12345 <richard.gao12345@gmail.com>
2018-07-18 17:11:22 -0700

    Boxes now expand on hover, added a temp header layout

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,28 +1,17 @@
 // //just testing this out
-$(document).ready(function(){
-    $('.port').mouseenter( function(){
-        $(this).animate({
-           width: "200%",
-           height: "200%"
-        }, 'fast');
-        $('person').animate({
-            width: "50%",
-            height: "50%"
-         }, 'slow');
-        $('prayer').animate({
-            width: "50%",
-            height: "50%"
-        }, 'slow');
-    });
-    $('.port').mouseleave( function(){
-        console.log("leaving")
-        $(this).animate({
-           width: "100%",
-           height: "100%"
-        }, 'fast');
-    });
+// $(document).ready(function(){
+//     $('.port').mouseenter( function(){
+//         $(this).stop().animate({
+//            height: "120%"
+//         }, 'fast');
+//     });
+//     $('.port').mouseleave( function(){
+//         $(this).stop().animate({
+//            height: "100%"
+//         }, 'fast');
+//     });
 
-});
+// });
 // $(document).ready(function(){
 //     $('.port').hover( function(){
 //         $(this).parent().css("grid-template-columns", "400px 100px");
