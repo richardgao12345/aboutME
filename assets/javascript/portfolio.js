81c021e5ee92f6ac80fd60d9ed8ec1aabed187d4
richardgao12345 <richard.gao12345@gmail.com>
2018-07-21 03:16:50 -0700

    Box doesn't expand to full screen, but text shows up

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -2,7 +2,7 @@ var modals = document.getElementsByClassName("box")
 var expandedModal = document.createElement('div')
 expandedModal.setAttribute('id', 'expandedModal')
 document.body.appendChild(expandedModal)
-var fullModal = document.getElementById('expandedModal');
+var fullModal = document.querySelector('#expandedModal');
 fullModal.style.position = "absolute";
 var position = {}
 var size = {}
@@ -22,7 +22,38 @@ var expandBox = function(event) {
             break;
         }
     }
+    size = {
+		width: window.getComputedStyle(currentModal).width,
+		height: window.getComputedStyle(currentModal).height
+    }
+    console.log(size)
     position = currentModal.getBoundingClientRect()
+	fullModal.style.top = position.top + 'px';
+	fullModal.style.left = position.left + 'px';
+	fullModal.style.height = size.height;
+	fullModal.style.width = size.width;
+    fullModal.style.margin = currentModal.style.margin;
+    
+    setTimeout(function(){
+        console.l
+		fullModal.innerHTML = currentModal.innerHTML;
+		var classes = currentModal.classList.value.split(' ');
+		for (var i = 0; i < classes.length; i++) {
+			fullModal.classList.add(classes[i]);
+        }
+        fullModal.style.position = "absolute";
+		fullModal.classList.add('growing');
+		fullModal.style.height = '100vh';
+		fullModal.style.width = '100vw';
+		fullModal.style.top = '0';
+		fullModal.style.left = '0';
+        fullModal.style.margin = '0';
+        console.log(fullModal)
+    }, 1);
+    setTimeout(function(){
+		fullModal.classList.remove('growing');
+		fullModal.classList.add('full-screen')
+	}, 1000);
     
 }
 for (var i = 0; i < modals.length; i++) {
