c574be9524419ed853c99c0d27e8fb431e55b054
richardgao12345 <richard.gao12345@gmail.com>
2018-07-28 13:30:05 -0700

    Can close box

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -8,7 +8,7 @@ var position = {}
 var size = {}
 var curModal = -1;
 
-
+//javascript function to expand the box
 var expandBox = function(event) {
     var currentModal = event.currentTarget;
     var classList = currentModal.classList;
@@ -43,18 +43,57 @@ var expandBox = function(event) {
 		fullModal.classList.add('growing');
 		fullModal.style.height = '80vh';
 		fullModal.style.width = '80vw';
-		fullModal.style.top = '0%';
+		fullModal.style.top = '5%';
 		fullModal.style.left = '10%';
         fullModal.style.margin = '0';
         fullModal.style.textAlign = 'center';
     }, 1);
     setTimeout(function(){
 		fullModal.classList.remove('growing');
-		fullModal.classList.add('full-screen')
+        fullModal.classList.add('full-screen')
 	}, 1000);
     
 }
 
-for (var i = 0; i < modals.length; i++) {
-    modals[i].addEventListener("click", expandBox)  
+//javscript function to close the box
+var closeBox = function(event) {
+    var $this = event.currentTarget;
+	$this.style.height = size.height;
+	$this.style.width = size.width;
+	$this.style.top = position.top + 'px';
+	$this.style.left = position.left + 'px';
+	$this.style.margin = '0';
+	$this.classList.remove('full-screen');
+	$this.classList.add('shrinking');
+	
+	setTimeout(function(){
+		while($this.firstChild) {
+            $this.removeChild($this.firstChild);
+        }
+		var classList = $this.classList;
+		while (classList.length > 0) {
+			 classList.remove(classList.item(0));
+		}
+		$this.style = '';;
+	}, 1000);
 }
+
+function linkClick(someID) {
+    switch (someID){
+        case "mail":
+            window.location.href = "mailto:rtrgao@ucdavis.edu";
+            break;
+        case "linkedin":
+            window.location.href = "//www.linkedin.com/in/richard-gao12345";
+            break;
+        case "github":
+            window.location.href = "//www.github.com/richardgao12345";
+            break;
+    }
+}
+
+for (var i = 0; i < modals.length; i++) {
+    modals[i].addEventListener("click", expandBox);
+}
+
+fullModal.addEventListener("click", closeBox);
